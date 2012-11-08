21314780141eacc28f26e529772fb5330fd21ab0
Pasi Kallinen <paxed@alt.org>
2012-11-08 18:07:19 +0200

    Fix showing searched strings with quotes once more

--- html.php
+++ html.php
@@ -96,7 +96,8 @@ function safe_str($s,$newch='%')
 
 function str_htmlize_quotes($str)
 {
-  $str = preg_replace("/(\\\"|\")/", "&quot;", $str);
+  $str = str_replace("\\\"", "&quot;", $str);
+  $str = str_replace("\"", "&quot;", $str);
   $str = str_replace("\\\\", "\\", $str);
   $str = str_replace("\\'", "'", $str);
   $str = str_replace("<", "&lt;", $str);
